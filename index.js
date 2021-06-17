5dad2ff38217dcfc18f0b6b6295df85f710b4644
shawna <shawna.akiona@gmail.com>
2021-06-16 18:58:58 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+   cats.push(Ralph);
+   return cats;
+}
+
+function destructivelyPrependCat(Bob) {
+   cats.unshift(Bob);
+   return cats;
+}
+
+function destructivelyRemoveLastCat() {
+   cats.pop();
+   return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+   cats.shift();
+   return cats;
+}
+
+function appendCat(Broom) {
+   var newCat = [...cats, Broom];
+   return newCat;
+}
+
+function prependCat(Arnold) {
+   var newCat = [Arnold,...cats];
+   return newCat;
+}
+
+function removeLastCat() {
+   var newArray = [...cats];
+   newArray.pop();
+   return newArray;
+}
+
+function removeFirstCat() {
+   var newArray = [...cats];
+   newArray.shift();
+   return newArray;
+}
